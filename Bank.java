4e97260e93d1bdec4f3bc24155ee10cd8e9fd358
beingnayem <nayemsarwar12345@gmail.com>
2025-06-17 04:49:27 +0600

    var 2

--- Bank.java
+++ Bank.java
@@ -29,10 +29,13 @@ public class Bank {
         
         ac.setPin(pin);
         this.signin=true;
-        
-        System.out.println("Account created succesfully.");
-        System.out.println("Account Holder Name: "+ac.name);
-        System.out.println("Account Number: "+ac.accontNo);
+        System.out.println("=======================================================");
+        System.out.print("\n");
+        System.out.println("            Account created succesfully.");
+        System.out.println("            Account Holder Name: "+ac.name);
+        System.out.println("            Account Number: "+ac.accontNo);
+        System.out.print("\n");
+        System.out.println("=======================================================");
         
         return ac;
     }
@@ -49,32 +52,52 @@ public class Bank {
     
         if(ac != null && pin.equals(ac.getPin())){
             this.signin = true;
-            System.out.println("Login succesfull.");
+            System.out.println("========================================");
+            System.out.print("\n");
+            System.out.println("            Login successful.");
+            System.out.print("\n");
+            System.out.println("========================================");
         }
         else{
-            System.out.println("Wrong Account No or PIN, try again.");
+            System.out.println("=====================================================");
+            System.out.print("\n");
+            System.out.println("            Wrong Account No or PIN, try again.");
+            System.out.print("\n");
+            System.out.println("=====================================================");
         }
         return ac;
     }
     public void logout(){
         this.signin = false;
-        System.out.println("Logout succesfull.");
+        System.out.println("========================================");
+        System.out.print("\n");
+        System.out.println("            Logout successful.");
+        System.out.print("\n");
+        System.out.println("========================================");
     }
     public void depositCash(Account ac){
         Scanner ob = new Scanner(System.in);
         System.out.print("Enter Deposit Ammount: ");
         Double ammount = ob.nextDouble();
         ac.diposit(ammount);
-        System.out.println("Deposit Successfull.");
-        System.out.println("Current balance is "+ac.getBalance());
+        System.out.println("=======================================================");
+        System.out.print("\n");
+        System.out.println("            Deposit Successful.");
+        System.out.println("            Current balance is "+ac.getBalance());
+        System.out.print("\n");
+        System.out.println("=======================================================");
     }
     public void withdrawCash(Account ac){
         Scanner ob = new Scanner(System.in);
         System.out.print("Enter Withdraw Ammount: ");
         Double ammount = ob.nextDouble();
         ac.withdraw(ammount);
-        System.out.println("Withdraw Successfull.");
-        System.out.println("Current balance is "+ac.getBalance());
+        System.out.println("=======================================================");
+        System.out.print("\n");
+        System.out.println("            Withdraw Successful.");
+        System.out.println("            Current balance is "+ac.getBalance());
+        System.out.print("\n");
+        System.out.println("=======================================================");
     }
     public Boolean changePin(Account ac){
         Scanner ob = new Scanner(System.in);
@@ -84,21 +107,79 @@ public class Bank {
             System.out.print("Enter New PIN: ");
             String newPin = ob.nextLine();
             ac.setPin(newPin);
-            System.out.println("New PIN is updated. Currently you are loged out, login again.");
             signin = false;
+
+            System.out.println("==================================================================================");
+            System.out.print("\n");
+            System.out.println("            New PIN is updated. Currently you are loged out, login again.");
+            System.out.print("\n");
+            System.out.println("==================================================================================");
+            
             return true;
         }
         else{
-            System.out.println("Current PIN is wrong, try again.");
+            System.out.println("====================================================");
+            System.out.print("\n");
+            System.out.println("            Current PIN is wrong, try again.");
+            System.out.print("\n");
+            System.out.println("====================================================");
             return false;
         }
     }
     public void getInfo(Account ac){
-        System.out.println("Account holder name: "+ac.name);
-        System.out.println("Account number: "+ac.accontNo);
-        System.out.println("Account holder Phone: "+ac.getPhone());
-        System.out.println("Current balance: "+ac.getBalance());
-        // System.out.println("Current PIN: "+ac.getPin());
+        System.out.println("=============================================================");
+        System.out.print("\n");
+        System.out.println("            Account holder name: "+ac.name);
+        System.out.println("            Account number: "+ac.accontNo);
+        System.out.println("            Account holder Phone: "+ac.getPhone());
+        System.out.println("            Current balance: "+ac.getBalance());
+        System.out.print("\n");
+        System.out.println("=============================================================");
+    }
+    public void fundTransfer(Account to){
+        Scanner ob = new Scanner(System.in);
+        System.out.print("Enter Reciver Account No: ");
+        int reciverAccountNO = ob.nextInt();
+        ob.nextLine();
+        Account reciver = accounts.get(reciverAccountNO);
+        if(reciver != null && to.accontNo != reciverAccountNO){
+            System.out.print("Enter Ammount: ");
+            Double ammount = ob.nextDouble();
+            System.out.print("Enter PIN: ");
+            ob.nextLine();
+            String pin = ob.nextLine();
+            if(pin.equals(to.getPin())){
+                reciver.diposit(ammount);
+                to.withdraw(ammount);
+                System.out.println("==========================================================================================");
+                System.out.print("\n");
+                System.out.println("            TK. " + ammount + "has been transfered to account "+reciver.accontNo);
+                System.out.print("\n");
+                System.out.println("==========================================================================================");
+            }
+            else{
+                System.out.println("====================================================================");
+                System.out.print("\n");
+                System.out.println("            Wrong PIN, try again.");
+                System.out.print("\n");
+                System.out.println("====================================================================");
+            }
+        }
+        else{
+            System.out.println("==================================================");
+            System.out.print("\n");
+            System.out.println("            Enter A Valid Reciver Account No.");
+            System.out.print("\n");
+            System.out.println("==================================================");
+        }
+        
+    }
+    public void checkBalance(Account ac){
+        System.out.println("================================================================================");
+        System.out.print("\n");
+        System.out.println("            Current Balance for account "+ac.accontNo+" is "+ac.getBalance());
+        System.out.print("\n");
+        System.out.println("================================================================================");
     }
 
     public static void main(String[] args){
@@ -111,7 +192,11 @@ public class Bank {
             System.out.print("Select option 1-3: ");
             int act = ob.nextInt();
             if(act == 3){
-                System.out.println("Thanks for using BBC Bank.");
+                System.out.println("==================================================");
+                System.out.print("\n");
+                System.out.println("            Thanks for using BBC Bank.");
+                System.out.print("\n");
+                System.out.println("==================================================");
                 break;
             }
             else if(act == 1 || act == 2){
@@ -131,11 +216,12 @@ public class Bank {
                     System.out.println("3. Withdraw Cash");
                     System.out.println("4. Change PIN");
                     System.out.println("5. Account Details");
-                    System.out.println("6. Logout");
-                    System.out.print("Select option 1-6: ");
+                    System.out.println("6. Fund Transfer");
+                    System.out.println("7. Logout");
+                    System.out.print("Select option 1-7: ");
                     int opt = ob.nextInt();
                     if(opt == 1){
-                        System.out.println("Current Balance for account "+ac.accontNo+" is "+ac.getBalance());
+                        bbcBank.checkBalance(ac);
                     }
                     else if(opt == 2){
                         bbcBank.depositCash(ac);
@@ -150,15 +236,18 @@ public class Bank {
                         bbcBank.getInfo(ac);
                     }
                     else if(opt == 6){
+                        bbcBank.fundTransfer(ac);
+                    }
+                    else if(opt == 7){
                         bbcBank.logout();
                     }
                     else{
-                        System.out.println("Wrong option, try again.");
+                        System.out.println("            Wrong option, try again.");
                     }
                 }
             }
             else{
-                System.out.println("Wrong option, try again.");
+                System.out.println("            Wrong option, try again.");
             }
         }
         ob.close();
